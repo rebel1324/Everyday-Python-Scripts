95d29a7bdf729270b0a3ae0ed35981e26931fd96
Kyu Yeon Lee <rebel1324@gmail.com>
2019-06-16 16:54:28 +0900

    Update workstart.py
    Removed win10 toast, it's basically worthless tbh

--- workstart.py
+++ workstart.py
@@ -1,15 +1,16 @@
+#-*- coding: utf-8 -*-
 # When I start coding or doing something, I usually forget to rest.
 # I just want to continue working on my stuffs.
+# man win10 toast is god damn worthless
 
 # Requires:
-#   win10toast
 #   python-telegram-bot
 
 import os
 import time
 import datetime
-import logging
-from win10toast import ToastNotifier
+from datetime import datetime, timedelta
+
 from telegram.ext import Updater
 from telegram.bot import Bot
 
@@ -21,7 +22,7 @@ class Telegram:
     def registerKey(self, path):
         api_key_dir = os.path.dirname(os.path.abspath(__file__))
         api_key_dir = os.path.join(api_key_dir, path)
-        
+
         try:
             f = open(api_key_dir, 'r')
             self.api_key = f.readline()
@@ -40,19 +41,22 @@ class Telegram:
     def sendMessage(self, message):
         self.bot.send_message(chat_id='538074142', text=message) #yeah it's my id buzz off
 
+restFirst = True
 if __name__ == "__main__":
-    toaster = ToastNotifier()
     tele = Telegram()
     tele.registerKey('telegram_key') #open telegram_key on script's path
     tele.checkValid()
 
     try:
         while True:
-            toaster.show_toast("일 시작!", "일을 시작할 시간이 되었습니다.", duration=10)
-            time.sleep(60*90) # 1.5 hours
+            if (not restFirst):
+                myTime = datetime.today() + timedelta(seconds = 60*90)
+                tele.sendMessage("일을 시작합니다. 다음 휴식 시간은 " + myTime.strftime("%H:%M:%S") + " 입니다.")
+                time.sleep(60*90) # 1.5 hours
             
-            toaster.show_toast("휴식 시간", "휴식을 취할 시간이 되었습니다.", duration=10)
+            myTime = datetime.today() + timedelta(seconds = 60*30)
+            tele.sendMessage("휴식을 시작합니다. 다음 일 시간은 " + myTime.strftime("%H:%M:%S") + " 입니다.")
             time.sleep(60*30) # 0.5 hours
-            tele.sendMessage("휴식 시간이 끝났습니다! 일을 해주세요")
+            restFirst = False
     except KeyboardInterrupt:
         print("일을 완전히 끝냅니다.")
